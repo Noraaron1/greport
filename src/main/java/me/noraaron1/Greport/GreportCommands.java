d8096a3b1ce269ec6c84e0a07ed95c0994300806
Noraaron1 <noraaronm@gmail.com>
2013-11-06 20:03:44 -0700

    Update GreportCommands.java

--- src/main/java/me/noraaron1/Greport/GreportCommands.java
+++ src/main/java/me/noraaron1/Greport/GreportCommands.java
@@ -63,7 +63,7 @@ public class GreportCommands extends JavaPlugin
     if (cmd.getName().equalsIgnoreCase("gread")) {
       if (player.hasPermission("gread.command")){
         try {
-          if (!isInt(args[0])) break label847;
+          if (!isInt(args[0])) break;
           try {
             ch.readReport(player, Integer.parseInt(args[0])); 
             } catch (NumberFormatException localNumberFormatException) {
